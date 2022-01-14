e26034b7a104ffa52d9989cf3e6eb1b5a2829a75
s3anmorrow <s3anmorrow@gmail.com>
2022-01-14 17:29:52 -0400

    stage setup

---
+++ src/Constants.ts
@@ -0,0 +1,3 @@
+// game constants
+export const STAGE_WIDTH:number = 600;
+export const STAGE_HEIGHT:number = 600;
---
+++ src/Game.ts
@@ -0,0 +1,27 @@
+// createjs typescript definition for TypeScript
+/// <reference path="./../node_modules/@types/createjs/index.d.ts" />
+
+// importing createjs framework
+import "createjs";
+// importing game constants
+import { STAGE_WIDTH, STAGE_HEIGHT } from "./Constants";
+
+// game variables
+let stage:createjs.StageGL;
+let canvas:HTMLCanvasElement;
+
+// --------------------------------------------------- main method
+function main():void {
+    // get reference to canvas
+    canvas = <HTMLCanvasElement> document.getElementById("game-canvas");
+    // set canvas width and height - this will be the stage size
+    canvas.width = STAGE_WIDTH;
+    canvas.height = STAGE_HEIGHT;    
+
+    // create stage object
+    stage = new createjs.StageGL(canvas, { antialias: true });
+
+
+}
+
+main();
